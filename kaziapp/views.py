2ac9cf3a67b2c8f674fa91368ac044abc3941ff8
kiptoo-097 <kiptoo097.com>
2020-12-09 01:08:13 +0300

    Added home view function to return index.html

--- kaziapp/views.py
+++ kaziapp/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .models import *
+from django.http import HttpResponse,Http404
 # Create your views here.
+
+
+
+def index(request):
+    applicant=Applicant.objects.all()
+    return render(request,'index.html',locals())
