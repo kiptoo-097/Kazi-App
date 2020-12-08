da417a1c2c975ed09b712cad01e99ad2b3458f8f
kiptoo-097 <kiptoo097.com>
2020-12-09 02:03:23 +0300

    Added class user and profile

--- kaziapp/models.py
+++ kaziapp/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 # Create your models here.
 class Applicant(models.Model):
@@ -26,3 +30,31 @@ class Applicant(models.Model):
     def search_by_job(cls,search_term):
         applicants = cls.objects.filter(job__icontains=search_term)
         return applicants
+
+class User(models.Model):
+    name=models.CharField(max_length=50,null=True)
+    user_email=models.EmailField(max_length=50,null=True)
+class Profile(models.Model):
+    name = models.CharField(max_length=70,null=True)
+    email = models.EmailField(null=True)
+    user = models.OneToOneField(User,on_delete=models.CASCADE,primary_key=True,related_name='profile')
+    profile_pic = models.ImageField(upload_to='image/', default='default.png')
+    bio=models.TextField(max_length=500,null=True )
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
+    def __str__(self):
+        return self.name
+
+
+    @classmethod
+    def all_profiles(cls):
+        profiles = cls.objects.all()
+        return profiles
